1c7ab1b9d7ddd16376f0ac663984429828d7235f
Calvin <calvinkioko91@gmail.com>
2022-09-29 22:14:22 +0300

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_handler
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_handler
+ 
+    private
+ 
+    def not_found_handler(e)
+       render json: { message: e.record.error }, status: :not_found
+    end
+ 
+    def invalid_handler(e)
+       render json: { messages: e.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
